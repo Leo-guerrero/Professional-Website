98e3cbaa7e09397363d63bcb2c88827c962035dd
Leo-guerrero <sja.leo.guerrero@gmail.com>
2024-07-12 15:42:08 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -38,3 +38,93 @@ window.addEventListener('DOMContentLoaded', () => {
         scrollingContent.appendChild(clone);
     });
 });
+
+const move = document.getElementById("move");
+
+document.body.onpointermove = event => {
+    const { clientX, clientY } = event;
+
+    move.animate({
+        left: `${clientX}px`,
+        top: `${clientY}px`
+    
+    }, {duration: 1000, fill: "forwards"})
+
+}
+
+/*
+const currentDate = new Date();
+console.log(currentDate);
+
+const background_picture = document.getElementById("main_backgroundID");
+background_picture.style.backgroundImage = "url('images/christmas_background.jpg')";
+*/
+var div = document.getElementById('github_text');
+div.onmouseover = function() {
+  const move2 = document.getElementById("move");
+  move2.style.padding = "1.8%";
+  document.getElementById("move").innerHTML = "view";
+}
+
+
+div.onmouseleave = function() {
+  const move2 = document.getElementById("move");
+  move2.style.padding = "0.5%";
+  document.getElementById("move").innerHTML = "";
+}
+
+
+
+var div2 = document.getElementById('view_scroll');
+div2.onmouseover = function() {
+  const move2 = document.getElementById("move");
+  move2.style.padding = "1.8%";
+  move2.style.color = "white";
+  document.getElementById("move").innerHTML = "Lets go!";
+}
+
+
+div2.onmouseleave = function() {
+  const move2 = document.getElementById("move");
+  move2.style.padding = "0.5%";
+  document.getElementById("move").innerHTML = "";
+}
+
+
+
+
+
+var div3 = document.getElementById('button_back');
+div3.onmouseover = function() {
+  const move2 = document.getElementById("move");
+  move2.style.padding = "1.8%";
+  move2.style.color = "black";
+  document.getElementById("move").innerHTML = "Back to Top";
+}
+
+
+div3.onmouseleave = function() {
+  const move2 = document.getElementById("move");
+  move2.style.padding = "0.5%";
+  document.getElementById("move").innerHTML = "";
+}
+
+
+
+var div4 = document.getElementById('link_buttons');
+div4.onmouseover = function() {
+  const move2 = document.getElementById("move");
+  move2.style.padding = "0%";
+}
+
+
+div4.onmouseleave = function() {
+  const move2 = document.getElementById("move");
+  move2.style.padding = "0.5%";
+}
+
+
+
+
+
+
